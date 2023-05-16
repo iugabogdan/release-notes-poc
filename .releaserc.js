0bef90688c52e83df7cfc6d736992538616a93ed
Bogdan Iuga <bogdan.iuga2@paddypowerbetfair.com>
2023-05-16 15:10:50 +0300

    feat(TPXX): Test new release message

--- .releaserc.js
+++ .releaserc.js
@@ -35,6 +35,9 @@ module.exports = {
           notifyOnFail: true,
           slackWebhook: 'https://hooks.slack.com/services/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',
           markdownReleaseNotes: true,
+          onSuccessTemplate: {
+            text: 'Package with version $npm_package_version of $package_name is now live on production. Below you can find the release notes.'
+          },
           branchesConfig: [
             {
               pattern: 'master',
